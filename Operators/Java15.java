bf04a915df0d69b5b37a2056bf0e136f7b6ef23c
Sakshi Borkar <166425641+SakshiBorkarSB@users.noreply.github.com>
2025-05-30 22:28:51 +0530

    Create Java15.java

---
+++ Operators/Java15.java
@@ -0,0 +1,18 @@
+// Arithmetic Operators in Java
+// BINARY OPERATORS
+
+import java.util.*;
+public class Java15{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int a = 10;
+        int b = 5;
+
+        System.out.println("a + b = " + (a+b));
+        System.out.println("a - b = " + (a-b));
+        System.out.println("a * b = " + (a*b));
+        System.out.println("a / b = " + (a/b));
+        System.out.println("a % b = " + (a%b));
+    }
+}
