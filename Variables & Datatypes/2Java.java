af57e02f06e34884b86f5b47acbaf6a869604b5a
Sakshi Borkar <166425641+SakshiBorkarSB@users.noreply.github.com>
2025-05-30 22:14:21 +0530

    Create 2Java.java

---
+++ Variables & Datatypes/2Java.java
@@ -0,0 +1,19 @@
+// Variables in Java
+
+public class 2Java{
+    public static void main(String args[]){
+        int a=10;
+        int b=20;
+        System.out.println(a);
+        System.out.println(b);
+
+        int c=a+b;
+        System.out.println(c);
+
+        String name="Sakshi Borkar";
+        System.out.println(name);
+
+        name="Sakshi Borkar";
+        System.out.println(name);
+    }
+}
