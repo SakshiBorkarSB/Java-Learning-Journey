ab0b5afbf6ffa13acb168c93a9256861ea4eba42
Sakshi Borkar <166425641+SakshiBorkarSB@users.noreply.github.com>
2025-05-30 22:16:49 +0530

    Create 3Java.java

---
+++ Variables & Datatypes/3Java.java
@@ -0,0 +1,20 @@
+// Datatypes in Java
+
+public class 3Java{
+    public static void main(String args[]){
+        byte b=8;
+        System.out.println(b);
+        char ch='s';
+        System.out.println(ch);
+        boolean var = true;
+        System.out.println(var);
+        float dec=3;
+        System.out.println(dec);
+        int num=7;
+        System.out.println(num);
+        long no=1233456789;
+        System.out.println(no);
+        double deci=1324.56739562;
+        System.out.println(deci);
+    }
+}
