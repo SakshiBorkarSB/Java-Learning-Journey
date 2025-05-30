add633dffae0f6be059fd2f7c3ea9f1a7dbe1aac
Sakshi Borkar <166425641+SakshiBorkarSB@users.noreply.github.com>
2025-05-30 22:22:23 +0530

    Create Java5.java

---
+++ Variables & Datatypes/Java5.java
@@ -0,0 +1,13 @@
+// Sum of two numbers
+
+import java.util.*;
+public class Java5{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the two numbers");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
