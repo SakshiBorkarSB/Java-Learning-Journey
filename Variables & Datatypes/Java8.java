2f5ad7b8dd05d98edca3dbcff46f05407a0de0b7
Sakshi Borkar <166425641+SakshiBorkarSB@users.noreply.github.com>
2025-05-30 22:23:45 +0530

    Create Java8.java

---
+++ Variables & Datatypes/Java8.java
@@ -0,0 +1,16 @@
+// Type Conversion in Java
+
+// Byte -> Short -> Int -> Float -> Long -> Double
+
+import java.util.*;
+public class Java8{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        // int number = sc.nextFloat;  
+        // NOT POSSIBLE
+
+        float number = sc.nextInt();
+        System.out.println(number);
+    }
+}
