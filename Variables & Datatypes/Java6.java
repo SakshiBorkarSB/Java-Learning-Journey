6fa41cc9183858a4712abe70454090e6927f8500
Sakshi Borkar <166425641+SakshiBorkarSB@users.noreply.github.com>
2025-05-30 22:22:51 +0530

    Create Java6.java

---
+++ Variables & Datatypes/Java6.java
@@ -0,0 +1,13 @@
+// Product of two numbers
+
+import java.util.*;
+public class Java6{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the two numbers");
+        float a =  sc.nextFloat();
+        float b =  sc.nextFloat();
+        float product = a * b;
+        System.out.println(product);
+    }
+}
