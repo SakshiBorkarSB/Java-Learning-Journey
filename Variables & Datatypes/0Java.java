0f645c9951e7ee7fcfb2313b6682449d1ee62621
Sakshi Borkar <166425641+SakshiBorkarSB@users.noreply.github.com>
2025-05-30 21:43:03 +0530

    Create 0Java.java

---
+++ Variables & Datatypes/0Java.java
@@ -0,0 +1,9 @@
+public class basics {
+   public basics() {
+   }
+
+   public static void main(String[] var0) {
+      System.out.println("Hello World!");
+      System.out.println("I am Sakshi Borkar");
+   }
+}
