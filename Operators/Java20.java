ee01ff61cf6f0074424c408ca2e7a4adbc6eb4c5
Sakshi Borkar <166425641+SakshiBorkarSB@users.noreply.github.com>
2025-05-31 16:56:35 +0530

    Create Java20.java

---
+++ Operators/Java20.java
@@ -0,0 +1,12 @@
+// What will be the output of the following program 
+
+public class Java20{
+    public static void main(String args[]){
+        int x = 2, y = 5;
+
+        int exp1 = (x * y / x);
+        int exp2 = (x * (y / x));
+
+        System.out.println(exp1 + "," + exp2);
+    }
+}
