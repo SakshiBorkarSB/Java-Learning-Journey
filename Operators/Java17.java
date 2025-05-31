dbc550cd067aa6249467d88b6c2cee158fafeba5
Sakshi Borkar <166425641+SakshiBorkarSB@users.noreply.github.com>
2025-05-31 16:55:27 +0530

    Create Java17.java

---
+++ Operators/Java17.java
@@ -0,0 +1,17 @@
+// Relational Operators
+
+import java.util.*;
+public class Java17{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = 10;
+        int b = 5;
+
+        System.out.println("a == b is " + (a==b));
+        System.out.println("a != b is " + (a!=b));
+        System.out.println("a > b is " + (a>b));
+        System.out.println("a < b is " + (a<b));
+        System.out.println("a >= b is " + (a>=b));
+        System.out.println("a <= b is " + (a<=b));
+    }
+}
