144163fbc314c643d39ab5a3406716d2fdab08ce
Sakshi Borkar <166425641+SakshiBorkarSB@users.noreply.github.com>
2025-05-31 16:55:02 +0530

    Create Java16.java

---
+++ Operators/Java16.java
@@ -0,0 +1,30 @@
+// Arithmetic Operators in Java
+// UNARY OPERATORS
+
+import java.util.*;
+public class Java16{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = 10;
+        int b = ++a;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        int c = 10;
+        int d = c++;
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+
+        int v = 10;
+        int x = --v;
+        System.out.println("v = " + v);
+        System.out.println("x = " + x);
+
+        int y = 10;
+        int z = y--;
+        System.out.println("y = " + y);
+        System.out.println("z = " + z);
+
+    
+    }
+}
