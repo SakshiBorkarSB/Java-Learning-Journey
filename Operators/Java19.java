31da31fcf58b3f0531cdab8089f1b351a74a025d
Sakshi Borkar <166425641+SakshiBorkarSB@users.noreply.github.com>
2025-05-31 16:56:11 +0530

    Create Java19.java

---
+++ Operators/Java19.java
@@ -0,0 +1,14 @@
+// Assignment Operator
+
+import java.util.*;
+public class Java19{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = 7;
+        System.out.println("a + 2 = " + (a+=2));
+        System.out.println("a - 2 = " + (a-=2));
+        System.out.println("a * 2 = " + (a*=2));
+        System.out.println("a / 2 = " + (a/=2));
+        System.out.println("a % 2 = " + (a%=2));
+    }
+}
