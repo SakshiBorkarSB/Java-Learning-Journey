49d04652374f9da019177671d317d1f2e6c9273a
Sakshi Borkar <166425641+SakshiBorkarSB@users.noreply.github.com>
2025-06-01 13:44:10 +0530

    Create Java31.java

---
+++ Conditional Statements/Java31.java
@@ -0,0 +1,23 @@
+// SWITCH CASE IN JAVA
+
+import java.util.*;
+public class Java31{
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        int number = 1;
+        switch(number)
+        {
+            case 1:
+                System.out.println("Shoto Todoroki");
+                break;
+            case 2:
+                System.out.println("Katsuki Bakugo");
+                break;
+            case 3:
+                System.out.println("Izuku Midoriya");
+                break;
+            default:
+                System.out.println("Delulu is the solulu");
+        }
+    }
+}
