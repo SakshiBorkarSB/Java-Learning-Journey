0bd305ae19f488a1a16d7f24213afde72e170873
Sakshi Borkar <166425641+SakshiBorkarSB@users.noreply.github.com>
2025-06-01 13:46:43 +0530

    Create Java37.java

---
+++ Loops/Java37.java
@@ -0,0 +1,13 @@
+// WHILE LOOP
+
+import java.util.*;
+public class Java37{
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        int loop = 1;
+        while (loop<=10){
+            System.out.println("Hello World!");
+            loop++;
+        }
+    }
+}
