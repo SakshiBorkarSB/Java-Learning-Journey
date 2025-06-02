7ec6cd5d24347aa750da7836b18265bdf36067da
Sakshi Borkar <166425641+SakshiBorkarSB@users.noreply.github.com>
2025-06-02 15:12:53 +0530

    Create Java46.java

---
+++ Loops/Java46.java
@@ -0,0 +1,14 @@
+// DO WHILE LOOP
+
+import java.util.*;
+public class Java46{
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        int i =1;
+
+        do{
+            System.out.println("Hello World!");
+            i++;
+        } while(i <= 7);
+    }
+}
