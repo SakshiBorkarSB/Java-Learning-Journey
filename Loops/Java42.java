be0b9adea56b29cb59761c5543f919bf03fadfdc
Sakshi Borkar <166425641+SakshiBorkarSB@users.noreply.github.com>
2025-06-02 15:11:39 +0530

    Create Java42.java

---
+++ Loops/Java42.java
@@ -0,0 +1,17 @@
+/* Print square pattern using for loop
+        *  *  *  *
+        *  *  *  *
+        *  *  *  *
+        *  *  *  *
+*/
+
+import java.util.*;
+public class Java42{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        for (int i = 1 ; i <= 4; i++){
+            System.out.println("*  *  *  *");
+        }
+    }
+}
