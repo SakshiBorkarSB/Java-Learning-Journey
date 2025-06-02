cc0983c91751ab149fc2a01a63c2a58b953a15f9
Sakshi Borkar <166425641+SakshiBorkarSB@users.noreply.github.com>
2025-06-02 15:11:22 +0530

    Create Java41.java

---
+++ Loops/Java41.java
@@ -0,0 +1,11 @@
+// FOR LOOP
+
+import java.util.*;
+public class Java41{
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        for(int i = 1 ; i <= 10 ; i++){
+            System.out.println("Hello World!");
+        }
+    }
+}
