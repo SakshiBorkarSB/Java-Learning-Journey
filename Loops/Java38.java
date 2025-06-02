98fddf5c5c93dd945cecef842dfae6f576fc26d7
Sakshi Borkar <166425641+SakshiBorkarSB@users.noreply.github.com>
2025-06-02 15:10:26 +0530

    Create Java38.java

---
+++ Loops/Java38.java
@@ -0,0 +1,13 @@
+// Print numbers from 1 to 10 using while loop
+
+import java.util.*;
+public class Java38{
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        int num = 1;
+        while (num<=10){
+            System.out.print(num + " ");
+            num++;
+        }
+    }
+}
