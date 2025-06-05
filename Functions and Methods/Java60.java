b093dff22095c6d3f9bd9130b1ed5e651dece8fe
Sakshi Borkar <166425641+SakshiBorkarSB@users.noreply.github.com>
2025-06-05 18:29:25 +0530

    Create Java60.java

---
+++ Functions and Methods/Java60.java
@@ -0,0 +1,18 @@
+// FUNCTIONS AND METHODS IN JAVA
+
+// METHODS - THE FUNCTIONS INSIDE THE CLASS ARE KNOWN AS METHODS
+
+public class Java60{
+    public static void printHelloWorld(){
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+    public static void main(String args[]){
+        printHelloWorld();    // function call
+    }
+}
