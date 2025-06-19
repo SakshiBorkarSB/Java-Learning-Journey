7c5158255a8c39a454752f92cdd48b85b91e8e72
Sakshi Borkar <166425641+SakshiBorkarSB@users.noreply.github.com>
2025-06-19 18:23:11 +0530

    Create Java132.java

---
+++ Strings/Java132.java
@@ -0,0 +1,42 @@
+// String Compression "aaabbcccdd"  =  "a3b2c3d2"
+
+public class Java132 {
+/* 
+    public static String compress(String str){
+        String newStr = "";
+        for(int i = 0; i < str.length(); i++){
+            Integer count = 1;
+            while(i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)){
+                count++;
+                i++;
+            }
+            newStr += str.charAt(i);
+            if(count > 1){
+                newStr += count.toString();
+            }
+        }
+        return newStr;
+    }
+*/
+
+    public static String compress(String str) {
+        StringBuilder newStr = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            int count = 1;
+            while (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+                i++;
+            }
+            newStr.append(str.charAt(i));
+            if (count > 1) {
+                newStr.append(count);
+            }
+        }
+        return newStr.toString();
+    }
+    public static void main(String args[]){
+        String str = "aaabbcccdd";
+
+        System.out.println(compress(str));
+    }
+}
