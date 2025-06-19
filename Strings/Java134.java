6985dc08653285efedb132177127642bcf24b3d9
Sakshi Borkar <166425641+SakshiBorkarSB@users.noreply.github.com>
2025-06-19 18:23:57 +0530

    Create Java134.java

---
+++ Strings/Java134.java
@@ -0,0 +1,8 @@
+// Replace in Java
+
+public class Java134 {
+        public static void main(String args[]) {
+        String str = "SakshiB".replace("B", "");
+        System.out.println(str);
+        }
+}
