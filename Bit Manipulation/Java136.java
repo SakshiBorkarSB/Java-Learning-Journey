6d15e73237e231a96eb420edeb5a3a042cc53f35
Sakshi Borkar <166425641+SakshiBorkarSB@users.noreply.github.com>
2025-06-22 15:05:42 +0530

    Create Java136.java

---
+++ Bit Manipulation/Java136.java
@@ -0,0 +1,14 @@
+// Bitwise operators     
+
+import java.util.*;
+
+public class Java136 {
+    public static void main(String args[]){
+        System.out.println(5 & 6);
+        System.out.println(5 | 6);
+        System.out.println(5 ^ 6);
+        System.out.println(~5);
+        System.out.println(5<<2);
+        System.out.println(6>>1);
+    }
+}
