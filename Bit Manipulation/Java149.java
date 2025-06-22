9a07e9db49166f4697df7b88a284ff811d5e3aee
Sakshi Borkar <166425641+SakshiBorkarSB@users.noreply.github.com>
2025-06-22 15:13:03 +0530

    Create Java149.java

---
+++ Bit Manipulation/Java149.java
@@ -0,0 +1,12 @@
+// Add 1 to an integer using Bit Manipulation.
+// (Hint : try using Bitwise NOT Operator)
+
+public class Java149 {
+    public static void main(String args[]){
+        int num = 6;
+        System.out.println(num + " + 1 = " + -~num);
+
+        num = -4;
+        System.out.println(num + " + 1 = " + -~num);
+    }    
+}
